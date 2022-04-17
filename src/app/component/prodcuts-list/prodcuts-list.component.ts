b151bde4273529a474b2ba81a60d55893a835740
Saleh <salehmoazy@gmail.com>
2022-04-17 08:35:43 +0300

    Created The prject added Matrial Design

---
+++ src/app/component/prodcuts-list/prodcuts-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prodcuts-list',
+  templateUrl: './prodcuts-list.component.html',
+  styleUrls: ['./prodcuts-list.component.css']
+})
+export class ProdcutsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
